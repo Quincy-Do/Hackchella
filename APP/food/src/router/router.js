fe7af79b10861a6476543fb84a7ad559a3e96148
kr3110 <n.nkr3110@gmail.com>
2018-04-22 02:18:32 -0400

    back arrow T.T

--- APP/food/src/router/router.js
+++ APP/food/src/router/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TabNavigator, StackNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator, HeaderBackButton } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 
 import login from '../screens/mainscreen'
@@ -7,19 +7,43 @@ import restaurants from '../screens/restaurants'
 import organizations from '../screens/organizations'
 
 
+const backButton = ({ navigation }) => ({
+    headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} />,
+})
+
+export const restaurantsNavigation = StackNavigator({
+  restaurantsNav: {
+    screen: restaurants,
+    navigationOptions: ({navigation}) => ({ //don't forget parentheses around the object notation
+           title: 'Welcome',
+           headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} />
+         })
+  },
+})
+export const organizationsNavigation = StackNavigator({
+  organizationsNav: {
+    screen: restaurants,
+    navigationOptions: ({navigation}) => ({ //don't forget parentheses around the object notation
+         title: 'Welcome',
+         headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} />
+       })
+  },
+})
 
 export const Root = StackNavigator({
   Login: {
-    screen: login
+    screen: login,
+    navigationOptions: {
+      header: null,
+    },
   },
-  Restaurants: {
-    screen: restaurants,
+  Restaurants:{
+    screen: restaurantsNavigation,
   },
-  Organizations: {
-    screen: organizations,
+  Organizations:{
+    screen: organizationsNavigation,
   },
 }, {
-  initialRouteName: 'Login',
   mode: 'modal',
   headerMode: 'none',
 });
